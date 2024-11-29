c2b553790b5c47a3b8f88aaec58eaa012d211f7f
Noah <82201000+Nopro263@users.noreply.github.com>
2024-11-30 00:32:58 +0100

    added Dragndrop

--- nbtLoader.js
+++ nbtLoader.js
@@ -1,14 +1,18 @@
 import { read } from "https://cdn.jsdelivr.net/npm/nbtify@2.1.0/+esm";
 
+export const loadArrayBuffer = async (arrayBuffer) => {
+    // Read the NBT binary with NBTify
+    const data = await read(arrayBuffer);
+
+    renderInventory(data.data.Inventory);
+}
+
 export const loadFile = async (url) => {
 
     const response = await fetch(url);
     const arrayBuffer = await response.arrayBuffer();
 
-    // Read the NBT binary with NBTify
-    const data = await read(arrayBuffer);
-
-    renderInventory(data.data.Inventory);
+    loadArrayBuffer(arrayBuffer);
 }
 
 const renderInventory = (inventory) => {
